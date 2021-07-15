e63aed7615c831b2ed2098632c6d37d0943fc5a0
agdaltarek <agdaltarek@gmail.com>
2021-07-15 19:49:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+})
+
+window.addEventListener('load', () => {
+    const welcome = document.querySelector('.welcome');
+    welcome.classList.add('welcome-after');
+})
